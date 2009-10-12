0a89a6660ba40ad859607d215ad56e3b5d065d17
Amit Rathore <amitrathore@gmail.com>
2009-10-12 15:32:43 -0700

    basic slime working with clojure

---
+++ amit.el
@@ -0,0 +1,19 @@
+(setq *MONITOR-WIDTH* 1920)
+
+(setq *FURTIVE-ROOT* "/Users/amit/workspace/furtive")
+(setq *SWARMIJI-ROOT* "/Users/amit/workspace/swarmiji")
+(setq *COMMON-JARS-ROOT* "/Users/amit/workspace/common-jars")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.sc
+  '(show-paren-mode t))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- init.el
+++ init.el
@@ -2,6 +2,30 @@
 
 (load-file (concat *EMACS-ENV* "/user-setup.el"))
 
+
 ;; clojure stuff
+(add-to-list 'load-path *EMACS-ENV*)
+(add-to-list 'load-path (concat *EMACS-ENV* "/packages/clojure-mode"))
+(add-to-list 'load-path (concat *EMACS-ENV* "/packages/swank-clojure"))
+(add-to-list 'load-path (concat *EMACS-ENV* "/packages/slime"))
+
+;; clojure-mode
 (add-to-list 'load-path (concat *EMACS-ENV* "/packages/clojure-mode"))
 (require 'clojure-mode)
+
+;; swank-clojure
+(add-to-list 'load-path (concat *EMACS-ENV* "/packages/swank-clojure/src/emacs"))
+
+(setq swank-clojure-jar-path (concat *EMACS-ENV* "/packages/clojure/clojure.jar")
+      swank-clojure-extra-classpaths (list
+				      (concat *EMACS-ENV* "/packages/swank-clojure/src/main/clojure")
+				      (concat *EMACS-ENV* "/packages/clojure-contrib/clojure-contrib.jar")))
+(require 'swank-clojure-autoload)
+
+;; slime
+(eval-after-load "slime"
+  '(progn (slime-setup '(slime-repl))))
+
+(add-to-list 'load-path (concat *EMACS-ENV*"/packages/slime"))
+(require 'slime)
+(slime-setup)
