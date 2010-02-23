4a1e08771f10c4d4fc4fa4d9d6e600c1ec98ebdd
Amit Rathore <amitrathore@gmail.com>
2010-02-23 14:37:36 -0800

    added some more stuff onto the swank path

--- custom/amit_paths.el
+++ custom/amit_paths.el
@@ -4,3 +4,22 @@
 (add-to-list 'extra-classpaths (concat *WORKSPACE* "/humpty-dumpty/src/"))
 (add-to-list 'extra-classpaths (concat *WORKSPACE* "/babble/src/"))
 (add-to-list 'extra-classpaths (concat *WORKSPACE* "/conjure/src/"))
+
+;;; the Remember project
+(add-to-list 'extra-classpaths (concat *WORKSPACE* "/remember/src/"))
+(setq REMEMBER-JARS
+      (directory-files (concat *WORKSPACE* "/remember/lib/java/") t "\\.jar$"))
+(mapcar 'add-to-extra-classpath REMEMBER-JARS)
+
+;;; the Clojure in Action project
+(add-to-list 'extra-classpaths "/Users/amit/labs/projectx/writing/manning/muse/muse-content/")
+(add-to-list 'extra-classpaths (concat *WORKSPACE* "/ring/src/"))
+(add-to-list 'extra-classpaths (concat *WORKSPACE* "/compojure/src/"))
+(add-to-list 'extra-classpaths (concat *WORKSPACE* "/clj-html/src/"))
+(add-to-list 'extra-classpaths (concat *WORKSPACE* "/enlive/src/"))
+
+;;; the Cognifly/Cratos
+(add-to-list 'extra-classpaths (concat *WORKSPACE* "/okula/cratos/src/"))
+(setq CRATOS-JARS
+      (directory-files (concat *WORKSPACE* "/okula/cratos/lib/java/") t "\\.jar$"))
+(mapcar 'add-to-extra-classpath CRATOS-JARS)
