7075d3b40c436d9ae42a94da8e1881d9f60e930c
Siva Jagadeesan <sivajag@gmail.com>
2011-01-27 15:28:19 -0800

    my changes

--- custom/siva_paths.el
+++ custom/siva_paths.el
@@ -1,17 +1,7 @@
-(add-to-list 'extra-classpaths "/Users/admin/work/clojure/playcode/src")
-(add-to-list 'extra-classpaths "/Users/admin/work/clojure/rsp/src")
-(add-to-list 'extra-classpaths "/Users/admin/work/clojure/accounts/src")
-(add-to-list 'extra-classpaths "/Users/admin/work/clojure/accounts/test")
-
-;Bobjure
-(add-to-list 'extra-classpaths "/Users/admin/work/clojure/bobjure/src")
-(add-to-list 'extra-classpaths "/Users/admin/work/clojure/bobjure/spec")
-
 (add-to-list 'extra-classpaths (concat *WORKSPACE* "/frumios/src/"))
 (add-to-list 'extra-classpaths (concat *WORKSPACE* "/frumios/spec/"))
 (add-to-list 'extra-classpaths (concat *WORKSPACE* "/damages/src/"))
 (add-to-list 'extra-classpaths (concat *WORKSPACE* "/humpty-dumpty/src/"))
-(add-to-list 'extra-classpaths (concat *WORKSPACE* "/babble/src/"))
 (add-to-list 'extra-classpaths (concat *WORKSPACE* "/conjure/src"))
 
 ;;; the Remember project
--- siva.el
+++ siva.el
@@ -11,12 +11,6 @@
 (setenv "SWARMIJI_ENV" "development")
 (setenv "SUMMARIZER_NS" "tesla")
 
-(setenv "FURTIVE_HOME" (concat (getenv "HOME") "/work/runa/furtive"))
-(setenv "FURTIVE_ENV" "development")
-(setenv "SWARMIJI_HOME" (concat (getenv "FURTIVE_HOME") "/lib/clojure/swarmiji"))
-(setenv "SWARMIJI_ENV" "development")
-(setenv "SUMMARIZER_NS" "tesla")
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -30,5 +24,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(set-default-font "-apple-Monaco-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1")
 
 (global-unset-key [(super p)])
